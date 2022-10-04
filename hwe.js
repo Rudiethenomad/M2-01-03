9fbce2ea6882300024279452a83bd78af990114a
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-04 21:10:18 +0200

    playing with the recipe

--- hwe.js
+++ hwe.js
@@ -6,14 +6,29 @@ let Guanciale = 250
 let Eggs = 6
 let Cheese = 50
 let Pepper = 4
+let Salt = "pinch"
+let Water = 3
+let grease = "SmallAmount"
 
-// Output:
+
+let SaltWater = Salt + Water;
+let largeBowl = Eggs + Cheese;
+let pan= largeBowl + Pepper;
+let largPan = SaltWater + Spaghetti;
+let fryingPan = grease;
+let ladel= SaltWater;
+let strainer = Spaghetti;
+
+
+Carbonara = fryingPan + pan + Guanciale + ladel + strainer;
+
 
 console.log("Spaghetti 400g", Spaghetti)
 console.log("guanciale 250g", Guanciale)
 console.log("VeryFreshEggYolks 6", Eggs)
 console.log("Cheese 50g", Cheese)
 console.log("Pepper 4g", Pepper)
+console.log (Carbonara)
 
 
 
