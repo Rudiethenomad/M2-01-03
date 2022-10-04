2dd1afb524b3372c45aee17f177ad5b88b7d56d9
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-04 17:34:57 +0200

    update

--- hw.js
+++ hw.js
@@ -18,15 +18,19 @@ MiddleName = "Willard";
 LastName = "Rudicil";
 Space = " "
 Greeting = "Hello, My name is";
+Age= " 122"
+
+
 
 
 console.log (Greeting + Space + FirstName + Space + MiddleName +Space + LastName)
 
 
 
-DateOfBirth = "1900";
+DateOfBirth = " 1900";
 
 CurrentYear = "2022"
 
 CurrentAge= "122"
 console.log (CurrentYear - DateOfBirth)
+console.log(CurrentYear - Age)
