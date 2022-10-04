3e6b214de4407b708d5b7c25802612b240e6882d
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2022-10-04 14:25:39 +0200

    Start of Java File
    This is the Homework I will try the extra

---
+++ hw.js
@@ -0,0 +1,30 @@
+
+
+
+let FirstName
+let  MiddleName
+let LastName 
+let DateOfBirth
+let Space
+let Greeting
+let Age
+let CurrentYear
+let id
+let CurrentAge
+
+FirstName = "Jon";
+MiddleName = "Willard";
+LastName = "Rudicil";
+Space = " "
+Greeting = "Hello, My name is";
+
+
+console.log (Greeting + Space + FirstName + Space + MiddleName +Space + LastName)
+
+
+
+DateOfBirth = "1968";
+CurrentAge = "54";
+CurrentYear = "2022"
+
+console.log (CurrentYear - CurrentAge)
